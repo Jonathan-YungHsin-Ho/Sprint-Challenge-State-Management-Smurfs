9ead326ad375bec65ea12b3a42cfb346b1510b2c
Jonathan Ho <johnnyho82@gmail.com>
2019-09-13 16:58:47 -0400

    Minor style adjustments

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -12,7 +12,7 @@ const App = () => {
       <div>Welcome to your state management version of Smurfs!</div>
       <div>Start inside of your `src/index.js` file!</div>
       <div>Have fun!</div> */}
-      <h1>WELSMURF TO SMURF VILLAGE!</h1>
+      <h1>Welsmurf to Smurf Village!</h1>
       <div className='forms-wrapper'>
         <SmurfForm />
         <span>or...</span>
